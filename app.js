fdb55bdc9aad5005caf53a68a514a751d8e7bd8b
smith <andras@sexybuddha.hu>
2020-12-28 09:33:12 +0100

    remove offending line from built js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-Object.defineProperty(exports, "__esModule", { value: true });
+
 const gamedig_1 = require("gamedig");
 const padStart = require('string.prototype.padstart');
 const Slimbot = require('slimbot');
